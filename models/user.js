0047d7bcadfc4e3397dea47d4dbb7d6aa3a796d5
DenisShamanskiy <75094596+DenisShamanskiy@users.noreply.github.com>
2021-07-28 23:50:12 +0300

    Добавлено models/user

---
+++ models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const NotAuthError = require('../errors/NotAuthError');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Поле "Имя" является обязательным'],
+    minlength: [2, 'Минимальная длина имени 2 символа'],
+    maxlength: [30, 'Максимальная длина имени 30 символов'],
+  },
+  email: {
+    type: String,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+      message: "Введен неверный формат E-mail",
+    },
+    required: [true, 'Поле "E-mail" является обязательным'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Поле "Пароль" является обязательным'],
+    select: false,
+  },
+});
+
+userSchema.statics.findUserByEmail = function (email, password) {
+  return this.findOne({ email })
+    .select("+password")
+    .orFail(() => new NotAuthError("Передан неверный логин или пароль"))
+    .then((user) =>
+      bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) throw new NotAuthError("Передан неверный логин или пароль");
+        return user;
+      })
+    );
+};
+
+module.exports = mongoose.model("user", userSchema);
