c5211d079422a7cb23ab62084822de5af90232fe
DenisShamanskiy <75094596+DenisShamanskiy@users.noreply.github.com>
2022-01-05 15:02:46 +0300

    Изменен PORT

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
-const MONGO_URL = 'mongodb://localhost:27017/bitfilmsdb';
+const MONGO_URL = "mongodb://localhost:27017/bitfilmsdb";
 
 module.exports = {
   PORT,
